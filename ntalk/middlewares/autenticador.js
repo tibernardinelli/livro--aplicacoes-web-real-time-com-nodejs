db9b4023735a1c553476e0ad979bf06c01e6d265
tbernardinelli <tbernardinelli@vanzolini-ead.org.br>
2014-07-30 13:32:59 -0300

    Chapter 5

---
+++ ntalk/middlewares/autenticador.js
@@ -0,0 +1,5 @@
+module.exports = function(req, res, next) {
+	if (!req.session.usuario)
+		return res.redirect('/');
+	return next();
+};
--- ntalk/routes/contatos.js
+++ ntalk/routes/contatos.js
@@ -1,9 +1,10 @@
 module.exports = function(app){
-	var contatos = app.controllers.contatos;
-	app.get('/contatos', contatos.index);
-	app.get('/contato/:id', contatos.show);
-	app.post('/contato', contato.create);
-	app.get('/contato/:id/editar', contato.edit);
-	app.put('/contato/:id', contato.update);
-	app.delete('/contato/:id', contato.destroy);
+	var autenticar = require('./../middlewares/autenticador')
+	, contatos = app.controllers.contatos;
+	app.get('/contatos', autenticar, contatos.index);
+	app.get('/contato/:id',  autenticar, contatos.show);
+	app.post('/contato', autenticar, contatos.create);
+	app.get('/contato/:id/editar', autenticar, contatos.edit);
+	app.put('/contato/:id', autenticar, contatos.update);
+	app.delete('/contato/:id', autenticar, contatos.destroy);
 };
