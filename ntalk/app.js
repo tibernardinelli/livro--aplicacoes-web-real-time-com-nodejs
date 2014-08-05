5cc1d6f30662f258092cbd0bffb0d76ab651f3f7
Tiago Canassa Bernardinelli <tiago.bernardinelli@gmail.com>
2014-08-04 23:57:39 -0300

    Correções de organização/correção socket.io

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,3 +1,4 @@
+const KEY = 'ntalk.sid', SECRET = 'ntalk';
 var express = require('express')
 	, load = require('express-load')
 	, bodyParser = require('body-parser')
@@ -7,34 +8,58 @@ var express = require('express')
 	, error = require('./middlewares/error')
     , app = express()
     , server = require('http').createServer(app)
-    , io = require('socket.io')(server);
+    , io = require('socket.io')(server)
+    , cookie = cookieParser(SECRET)
+    , store = new expressSession.MemoryStore();
 	
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(cookieParser('ntalk'));
-app.use(expressSession());
+app.use(cookie);
+app.use(expressSession({
+	secret: SECRET,
+	name: KEY,
+	resave: true,
+	saveUninitialized: true,
+	store: store
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'));
+
+io.use(function(socket, next) {
+	var data = socket.request;
+	console.log(data);
+	cookie(data, {}, function(err) {	
+		var sessionID = data.signedCookies[KEY];		
+		console.log(sessionID);
+		store.get(sessionID, function(err, session) {
+			if (err || session) {
+				console.log('a');
+				return next(new Error('Acesso negado'));
+			} else {
+				console.log('b');
+				socket.handshake.session = session;
+				return next();
+			}
+		});
+	});
+});
+
 
 load('models')
     .then('controllers')
     .then('routes')
     .into(app);
 
+load('sockets')
+	.into(io);
+
+
 app.use(error.notFound);
 app.use(error.serverError);
-
-
-io.sockets.on('connection', function(client) {
-	client.on('send-server', function(data) {
-		var msg = 	"<b>" + data.nome + ":</b> " + data.msg + "<br>";
-		client.emit('send-client', msg);
-		client.broadcast.emit('send-client', msg);
-	});
-});
 
 server.listen(3000, function(){
     console.log("Ntalk no ar.");
